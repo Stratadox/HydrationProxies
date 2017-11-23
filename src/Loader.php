006b44ffa383f17f471e5b180469c417fcdddab9
Stratadox <jesse@stratadox.com>
2017-11-23 21:49:45 +0100

    Move observable loader behaviour to abstract class

--- src/Loader.php
+++ src/Loader.php
@@ -4,7 +4,37 @@ declare(strict_types=1);
 
 namespace Stratadox\Hydration\Proxying;
 
-class Loader
-{
+use Stratadox\Hydration\LoadsProxiedObjects;
+use Stratadox\Hydration\ObservesProxyLoading;
 
+abstract class Loader implements LoadsProxiedObjects
+{
+    /** @var ObservesProxyLoading[] */
+    private $observers;
+
+    public function attach(ObservesProxyLoading $observer) : void
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function detach(ObservesProxyLoading $observer) : void
+    {
+        unset($this->observers[array_search($observer, $this->observers, true)]);
+    }
+
+    final public function loadTheInstance()
+    {
+        $instance = $this->doLoadTheInstanceDearest();
+        $this->tellThemWeMadeThis($instance);
+        return $instance;
+    }
+
+    protected function tellThemWeMadeThis($instance)
+    {
+        foreach ($this->observers as $observer) {
+            $observer->updateWith($instance);
+        }
+    }
+
+    protected abstract function doLoadTheInstanceDearest();
 }
--- tests/Foo/FooLoader.php
+++ tests/Foo/FooLoader.php
@@ -4,35 +4,12 @@ declare(strict_types=1);
 
 namespace Stratadox\Hydration\Proxying\Test\Foo;
 
-use Stratadox\Hydration\LoadsProxiedObjects;
-use Stratadox\Hydration\ObservesProxyLoading;
+use Stratadox\Hydration\Proxying\Loader;
 
-class FooLoader implements LoadsProxiedObjects
+class FooLoader extends Loader
 {
-    /** @var ObservesProxyLoading[] */
-    private $observers;
-
-    public function attach(ObservesProxyLoading $observer) : void
+    protected function doLoadTheInstanceDearest()
     {
-        $this->observers[] = $observer;
-    }
-
-    public function detach(ObservesProxyLoading $observer) : void
-    {
-        unset($this->observers[array_search($observer, $this->observers, true)]);
-    }
-
-    public function loadTheInstance()
-    {
-        $instance = new Foo();
-        $this->tellThemWeMadeThis($instance);
-        return $instance;
-    }
-
-    protected function tellThemWeMadeThis($instance)
-    {
-        foreach ($this->observers as $observer) {
-            $observer->updateWith($instance);
-        }
+        return new Foo;
     }
 }
