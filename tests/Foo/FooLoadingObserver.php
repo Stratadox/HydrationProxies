54869eb0dee36c7f1a04c513a7cc8bdd1e143727
Stratadox <jesse@stratadox.com>
2017-11-23 21:43:45 +0100

    Add test for proxied object loading

---
+++ tests/Foo/FooLoadingObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Stratadox\Hydration\Proxying\Test\Foo;
+
+use Stratadox\Hydration\ObservesProxyLoading;
+
+class FooLoadingObserver implements ObservesProxyLoading
+{
+    private $instance;
+
+    public function updateWith($theLoadedInstance) : void
+    {
+        $this->instance = $theLoadedInstance;
+    }
+
+    public function instance()
+    {
+        return $this->instance;
+    }
+}
---
+++ tests/Unit/Loader_updates_observers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Stratadox\Hydration\Proxying\Test;
+
+use PHPUnit\Framework\TestCase;
+use Stratadox\Hydration\Proxying\Test\Foo\FooLoader;
+use Stratadox\Hydration\Proxying\Test\Foo\FooLoadingObserver;
+
+/**
+ * @covers \Stratadox\Hydration\Proxying\Loader
+ */
+class Loader_updates_observers extends TestCase
+{
+    /** @scenario */
+    function update_with_result_after_loading()
+    {
+        $observer = new FooLoadingObserver;
+        $loader = new FooLoader;
+
+        $loader->attach($observer);
+        $foo = $loader->loadTheInstance();
+
+        $this->assertSame($foo, $observer->instance());
+    }
+
+    /** @scenario */
+    function do_not_update_before_loading()
+    {
+        $observer = new FooLoadingObserver;
+        $loader = new FooLoader;
+
+        $loader->attach($observer);
+
+        $this->assertNull($observer->instance());
+    }
+
+    /** @scenario */
+    function do_not_update_if_detached()
+    {
+        $observer = new FooLoadingObserver;
+        $loader = new FooLoader;
+
+        $loader->attach($observer);
+        $loader->detach($observer);
+        $loader->loadTheInstance();
+
+        $this->assertNull($observer->instance());
+    }
+}
